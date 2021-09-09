e5580b30d83cc73e82901c795535aef6e8f6a9d9
Craig Puxty <craig_puxty@talktalk.net>
2021-09-09 20:28:28 +0100

    Updated Header

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,14 +1,20 @@
 function Header() {
+  const ScrollToSection = () => {};
   return (
     <header className="header">
       <div className="container">
-        <div className="subheading">Frontend Developer</div>
+        <div className="subheading">Front-end Developer.</div>
         <h1>Craig Puxty</h1>
-        <p>
+        <p className="lead">
           Projects created with <span>p</span>assion, producing great{" "}
           <span>u</span>ser e<span>x</span>periences!
         </p>
-        <button className="btn btn--primary btn--large">Recent Projects</button>
+        <button
+          className="btn btn--secondary btn--large"
+          onClick={ScrollToSection}
+        >
+          Recent Projects
+        </button>
       </div>
     </header>
   );
