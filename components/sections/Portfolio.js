6af8b45f6990004413ac28d4d5871bbcf2060c80
Craig Puxty <craig_puxty@talktalk.net>
2021-10-17 16:22:28 +0100

    Added Project links for Portfolio section

--- components/sections/Portfolio.js
+++ components/sections/Portfolio.js
@@ -1,5 +1,6 @@
 import myProjects from "../../data/myProjects";
 import Image from "next/image";
+import Link from "next/link";
 
 function Portfolio() {
   return (
@@ -9,24 +10,28 @@ function Portfolio() {
         <h2 className="section__title center">Recent Projects</h2>
         <div className="section__block grid grid--multiple">
           {myProjects.map((project) => (
-            <div className="project" key={`project-${project.id}`}>
-              <div className="project__image">
-                <Image
-                  src={project.preview_image}
-                  alt={project.title}
-                  width="265"
-                  height="150"
-                  layout="responsive"
-                  blurDataURL={project.preview_image}
-                  placeholder="blur"
-                />
-              </div>
-              <div className="project__details">
-                <div className="project__type">{project.type}</div>
-                <h4 className="project__title">{project.title}</h4>
-                <div className="project__year">{project.date}</div>
-              </div>
-            </div>
+            <Link href={`/project/${project.id}`} key={`project-${project.id}`}>
+              <a>
+                <div className="project">
+                  <div className="project__image">
+                    <Image
+                      src={project.preview_image}
+                      alt={project.title}
+                      width="265"
+                      height="150"
+                      layout="responsive"
+                      blurDataURL={project.preview_image}
+                      placeholder="blur"
+                    />
+                  </div>
+                  <div className="project__details">
+                    <div className="project__type">{project.type}</div>
+                    <h4 className="project__title">{project.title}</h4>
+                    <div className="project__year">{project.date}</div>
+                  </div>
+                </div>
+              </a>
+            </Link>
           ))}
         </div>
       </div>
