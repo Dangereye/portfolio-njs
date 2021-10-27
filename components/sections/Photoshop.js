1c1773f1038571c3827c6dbb0a76fff2de4ce875
Craig Puxty <craig_puxty@talktalk.net>
2021-10-27 21:58:05 +0100

    Removed obsolete SlidesShow module

--- components/sections/Photoshop.js
+++ components/sections/Photoshop.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Slideshow from "./SlideShow";
 import slideshowData from "/data/slideshowData";
 
 function Photoshop({ setSlideshowIsOpen, setFirstSlide }) {
